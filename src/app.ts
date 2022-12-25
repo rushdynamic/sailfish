5b90031256087b32959ec55d8862d93a0a178585
rushdynamic <tameimpalafanclubpresident@gmail.com>
2022-12-25 15:38:40 +0530

    feat: basic express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (_, res) => {
+    res.send('Hello, World!');
+});
+
+server.listen(3000, () => {
+    console.log('Server started listening on port 3000');
+});
