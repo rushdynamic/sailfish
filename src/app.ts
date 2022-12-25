3f4197c4ea1d2777f4847363578f81ecb0e1a31e
rushdynamic <tameimpalafanclubpresident@gmail.com>
2022-12-25 15:55:15 +0530

    feat: basic socket.io server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+const socketServer = new Server(server);
 
 app.get('/', (_, res) => {
     res.send('Hello, World!');
+});
+
+socketServer.on('connection', (socket) => {
+    console.log('A socket has connected:' + socket);
 });
 
 server.listen(3000, () => {
